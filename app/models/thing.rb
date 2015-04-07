73507c070733f02c015a40e321d687e74803759f
Adam Louis <adamlouis@gmail.com>
2015-04-06 19:33:57 -0700

    initial simple pagination

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,2 +1,8 @@
 class Thing < ActiveRecord::Base
+  def self.paginate(options = {})
+    per_page = (options[:per_page] || 30).to_i
+    page     = (options[:page]     || 1).to_i
+    offset   = (page - 1) * per_page
+    self.offset(offset).limit(per_page)
+  end
 end
