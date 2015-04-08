22f7741daeb1717bcd9246a4e269714773b219cd
Adam Louis <adamlouis@gmail.com>
2015-04-07 20:07:25 -0700

    store pagination on scope

--- lib/pagination.rb
+++ lib/pagination.rb
@@ -1,8 +1,27 @@
 module Pagination
-  def paginate(options = {})
-    per_page = (options[:per_page] || 30).to_i
-    page     = (options[:page]     || 1).to_i
-    offset   = (page - 1) * per_page
-    self.offset(offset).limit(per_page)
+  class Pagination
+    attr_reader :page, :per_page
+
+    def initialize(options)
+      @per_page   = (options[:per_page] || 30).to_i
+      @page       = (options[:page]     || 1).to_i
+    end
+
+    def offset
+      (self.page - 1) * self.per_page
+    end
+
+    def paginate(scope)
+      scope.pagination = self
+      scope.offset(self.offset).limit(self.per_page)
+    end
   end
+
+  def paginate(options = {})
+    Pagination.new(options).paginate(self.all)
+  end
+end
+
+class ActiveRecord::Relation
+  attr_accessor :pagination
 end
