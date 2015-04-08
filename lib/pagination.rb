b3d78514a5bafbb3ec06ae60bc7b52e7ea0844d9
Adam Louis <adamlouis@gmail.com>
2015-04-07 20:26:44 -0700

    pagination remembers incoming scope's total count

--- lib/pagination.rb
+++ lib/pagination.rb
@@ -1,6 +1,6 @@
 module Pagination
   class Pagination
-    attr_reader :page, :per_page
+    attr_reader :page, :per_page, :total
 
     def initialize(options)
       @per_page   = (options[:per_page] || 30).to_i
@@ -11,10 +11,15 @@ module Pagination
       (self.page - 1) * self.per_page
     end
 
+    def total_pages
+      self.total.fdiv(self.per_page).ceil
+    end
+
     def paginate(scope)
       class << scope
         attr_accessor :pagination
       end
+      @total = scope.count
       scope.pagination = self
       scope.offset(self.offset).limit(self.per_page)
     end
